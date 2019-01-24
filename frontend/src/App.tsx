263af4a0d7d0817da0f894166a428c3810a560e4
Oscar3001 <oscar@dbbsoftware.com>
2019-01-24 10:31:56 +0200

    Added Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,23 @@ import {BrowserRouter,Route,Redirect,Switch} from 'react-router-dom';
 import AuthPage from './Pages/Auth';
 import EventsPage from './Pages/Events';
 import BookingsPage from './Pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 class App extends React.Component {
   public render() {
     return (
       <BrowserRouter>
+      <React.Fragment>
+        <MainNavigation />
+        <main className="main-content">
         <Switch>
           <Redirect from="/" to="/auth" exact />
           <Route path="/auth" component={AuthPage} />
           <Route path="/events" component={EventsPage} />
           <Route path="/bookings" component={BookingsPage} />
         </Switch>
+        </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
--- frontend/src/components/Navigation/MainNavigation.tsx
+++ frontend/src/components/Navigation/MainNavigation.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
+import {NavLink} from 'react-router-dom';
+
+import './MainNavigation.css';
 
 function MainNavigation(props:any){
   return (
-    <div>
-      <h1>The Navbar</h1>
-    </div>
+    <header className="main-navigation">
+      <div className="main-navigation-logo">
+        <h1>EasyEvent</h1>
+      </div>
+      <nav className="main-navigation-items">
+        <ul>
+          <li>
+            <NavLink to="/auth">Authenticate</NavLink>
+          </li>
+          <li>
+            <NavLink to="/events">Events</NavLink>
+          </li>
+          <li>
+            <NavLink to="/bookings">Bookings</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
